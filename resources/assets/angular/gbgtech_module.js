ad8d0ff1bd887a5994647de9987ad7fbd6ed4810
Aleksandar Faraj <aleksandarfaraj@OS-X.local>
2015-05-02 16:14:24 +0200

    Added Google App apikey

--- resources/assets/angular/gbgtech_module.js
+++ resources/assets/angular/gbgtech_module.js
@@ -1,1 +1,15 @@
-var gbgtech = angular.module('gbgtech', []);
+var gbgtech = angular.module('gbgtech', ['gapi'])
+.value('GoogleApp', {
+    apiKey: 'AIzaSyA-WngRqzV1hbbyTQd-OiXlKigPIcZbuBg',
+    clientId: 'YOUR_CLIENT_ID',
+    scopes: [
+      // whatever scopes you need for your app, for example:
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/calendar.readonly',
+      'https://www.googleapis.com/auth/youtube',
+      'https://www.googleapis.com/auth/userinfo.profile'
+      // ...
+    ]
+  }).
+controller('GbgTechController',['$scope',function($scope){
+}]);
