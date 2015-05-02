6498d670e7c6d8193f89accab76c3af99677f156
Erik Axelsson <erikaxelsson1@gmail.com>
2015-05-02 17:35:58 +0200

    add get all calenders

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,5 +1,7 @@
 <?php namespace App\Http\Controllers;
 
+use Config;
+use Cache;
 class WelcomeController extends Controller {
 
 	/*
@@ -30,7 +32,41 @@ class WelcomeController extends Controller {
 	 */
 	public function index()
 	{
+
 		return view('welcome');
 	}
+    public function getListOfEvents(){
+        /* Get config variables */
+        $client_id = Config::get('google.client_id');
+        $service_account_name = Config::get('google.service_account_name');
+        $key_file_location = base_path() . Config::get('google.key_file_location');
+
+        $this->client = new \Google_Client();
+        $this->client->setApplicationName("Your Application Name");
+        $this->service = new \Google_Service_Calendar($this->client);
+
+        /* If we have an access token */
+        if (Cache::has('service_token')) {
+            $this->client->setAccessToken(Cache::get('service_token'));
+        }
+
+        $key = file_get_contents($key_file_location);
+        $scopes = array('https://www.googleapis.com/auth/calendar');
+        $cred = new \Google_Auth_AssertionCredentials(
+            $service_account_name,
+            $scopes,
+            $key
+        );
+
+        $this->client->setAssertionCredentials($cred);
+        if ($this->client->getAuth()->isAccessTokenExpired()) {
+            $this->client->getAuth()->refreshTokenWithAssertion($cred);
+        }
+        Cache::forever('service_token', $this->client->getAccessToken());
+
+
+        $results = $this->service->events->listEvents("6e77ksd17iuk82r0j0t7bjdbe8@group.calendar.google.com");
+        echo json_encode($results->getItems());
+    }
 
 }
---
+++ config/google.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client ID
+    |--------------------------------------------------------------------------
+    |
+    | The Client ID can be found in the OAuth Credentials under Service Account
+    |
+    */
+    'client_id' => '954748189572-aqsej5gnumu1kp6as1cf60uldq3o95vr.apps.googleusercontent.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Service account name
+    |--------------------------------------------------------------------------
+    |
+    | The Service account name is the Email Address that can be found in the
+    | OAuth Credentials under Service Account
+    |
+    */
+    'service_account_name' => '954748189572-aqsej5gnumu1kp6as1cf60uldq3o95vr@developer.gserviceaccount.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Key file location
+    |--------------------------------------------------------------------------
+    |
+    | This is the location of the .p12 file from the Laravel root directory
+    |
+    */
+    'key_file_location' => '/resources/assets/gbgtech-cab3775b9262.p12',
+];
